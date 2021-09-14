b52f2d588593ea7cb298a0f2a5248e212588957a
admin <admin@YD02-11>
2021-09-14 11:17:45 +0900

    second

--- firstProject/src/firstProject/HelloWorld.java
+++ firstProject/src/firstProject/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
    public static void main(String[] args) {
 	   Scanner scn = new Scanner(System.in);
 	  System.out.println("Hello World");
+	  System.out.println("What u r name?");
 	  String name = scn.next();
 	  System.out.println("Welcome "+name);
    }
